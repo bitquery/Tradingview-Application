5c0fbfb9bbeded023e82a3e0d848fbf9ac673bb3
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2023-05-16 14:25:36 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import {TradingViewData} from './bitquery'
+function App() {
+  return (
+    <div className="App">
+      <TradingViewData/>
+    </div>
+  );
+}
+
+export default App;
